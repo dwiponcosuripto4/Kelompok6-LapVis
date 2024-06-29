06f03135d314e982178d76820cd4ce35268b2de2
dwiponcosuripto4 <dwiponcosuripto44@gmai.com>
2024-06-29 07:08:00 +0700

    fix theme button

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,37 +10,36 @@ Alpine.start();
 var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!("color-theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-    themeToggleLightIcon.classList.remove("hidden");
-} else {
-    themeToggleDarkIcon.classList.remove("hidden");
+function updateIcons() {
+    if (document.documentElement.classList.contains('dark')) {
+        themeToggleLightIcon.classList.remove("hidden");
+        themeToggleDarkIcon.classList.add("hidden");
+    } else {
+        themeToggleDarkIcon.classList.remove("hidden");
+        themeToggleLightIcon.classList.add("hidden");
+    }
 }
+
+// Check for theme preference at the beginning
+if (localStorage.getItem("color-theme") === "dark" || 
+    (!localStorage.getItem("color-theme") && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem("color-theme", "dark");
+} else {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem("color-theme", "light");
+}
+
+updateIcons();
 
 var themeToggleBtn = document.getElementById("theme-toggle");
 
 themeToggleBtn.addEventListener("click", function () {
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
-
-    if (localStorage.getItem("color-theme")) {
-        if (localStorage.getItem("color-theme") === "light") {
-            document.documentElement.classList.add("dark");
-            localStorage.setItem("color-theme", "dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-            localStorage.setItem("color-theme", "light");
-        }
+    document.documentElement.classList.toggle('dark');
+    if (document.documentElement.classList.contains('dark')) {
+        localStorage.setItem("color-theme", "dark");
     } else {
-        if (document.documentElement.classList.contains("dark")) {
-            document.documentElement.classList.remove("dark");
-            localStorage.setItem("color-theme", "light");
-        } else {
-            document.documentElement.classList.add("dark");
-            localStorage.setItem("color-theme", "dark");
-        }
+        localStorage.setItem("color-theme", "light");
     }
+    updateIcons();
 });
